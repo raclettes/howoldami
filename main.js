4cd4326b3bf6546fb4dee4c22ace95bf2a8075ac
olegispe <olegispe@gmail.com>
2018-10-28 15:13:57 +0000

    oops

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ function show() {
   // Milliseconds, Seconds, Minutes, hours, days, years.
 
   setInterval(function () {
-    if (iteration++ % 1000 === 0) {
-      type++
-    }
-
     var now = new Date()
     var then = new Date(parseInt(document.getElementById('year').value), parseInt(document.getElementById('month').value) - 1, parseInt(document.getElementById('day').value))
-
-    var time = new Date(now - then)
-
+    
+    if (iteration == 1) {
+      console.log(now)
+      console.log(then)
+      console.log(now.getFullYear() - then.getFullYear())
+    }
+    
+              if (iteration++ % 1000 === 0) {
+                type++
+              }
+    var time = now - then
     switch (type % 5) {
       // case 0:
       //   document.getElementById('cd').innerText = `${time * 1} milliseconds`
@@ -34,7 +38,7 @@ function show() {
         document.getElementById('cd').innerText = `${(time / (1000 * 60 * 60 * 24)).toFixed(3)} days`
         break
       case 4:
-        document.getElementById('cd').innerText = `${(time / (1000 * 60 * 60 * 24 * 356)).toFixed(3)} years`
+        document.getElementById('cd').innerText = `${(time / (1000 * 60 * 60 * 24 * 365)).toFixed(3)} years`
         break
       default:
         break;
